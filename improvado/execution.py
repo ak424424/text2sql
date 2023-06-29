3897b525b0020c4e7c55c58e4dea4ed136d92e3b
ak424424 <119412213+ak424424@users.noreply.github.com>
2023-06-30 01:25:25 +0600

    Update execution.py

--- improvado/execution.py
+++ improvado/execution.py
@@ -5,7 +5,7 @@ main_model = MainModel()
 
 def text2sql_prototype():
     q, t, cols = chat_part.chat_like_part()
-    sql_draft = main_model.llm_chain.run({"Table": t, "question": q, "Columns": cols})
+    sql_draft = main_model.get_llm_response(t, q, cols)
     return sql_draft, client.execute(sql_draft)
 
 if __name__ == '__main__':
